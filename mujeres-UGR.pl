801fec060714b8c028f6660c336f3690b870c83b
JJ Merelo <jjmerelo@gmail.com>
2017-02-11 13:00:31 +0100

    Prueba en Perl

---
+++ mujeres-UGR.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use URI::Encode qw(uri_encode);
+use LWP::Simple;
+
+use v5.20;
+
+my $query=<<EOC;
+SELECT ?person ?personLabel WHERE {
+  
+    ?person wdt:P69 wd:Q1232180 . 
+    ?person wdt:P21 wd:Q6581072 . 
+  
+    SERVICE wikibase:label { 
+      bd:serviceParam wikibase:language "en" .
+    }
+} ORDER BY ?personLabel
+EOC
+
+
+my $result = get("https://query.wikidata.org/sparql?format=JSON\&query=".uri_encode($query));
+
+say $result;
